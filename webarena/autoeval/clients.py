a7dfee2b3b8d18932de95ade9f2f4be6cf015a32
Vishwa Shah <52594777+vishwa27yvs@users.noreply.github.com>
2024-09-16 22:23:43 -0400

    (contd.) HF Inf client in AutoEval

--- webarena/autoeval/clients.py
+++ webarena/autoeval/clients.py
@@ -5,14 +5,19 @@ import numpy as np
 from PIL import Image
 from typing import Union, Optional
 from openai import OpenAI, ChatCompletion
-openai.api_key = os.environ["OPENAI_API_KEY"]
-openai.organization = os.environ.get("OPENAI_ORGANIZATION", "")
-client = OpenAI()
-
+from huggingface_hub import InferenceClient
 
 class LM_Client:
     def __init__(self, model_name: str = "gpt-3.5-turbo") -> None:
         self.model_name = model_name
+        if "gpt" in model_name:
+            openai.api_key = os.environ["OPENAI_API_KEY"]
+            openai.organization = os.environ.get("OPENAI_ORGANIZATION", "")
+            self.client = OpenAI()
+        else:
+            # Assuming HF Inf models by default
+            print(f"HF Inf model load: {self.model_name}")
+            self.client = InferenceClient(self.model_name, token = os.environ.get('HF_TOKEN'))
 
     def chat(self, messages, json_mode: bool = False) -> tuple[str, ChatCompletion]:
         """
@@ -21,14 +26,24 @@ class LM_Client:
             {"role": "user", "content": "hi"},
         ])
         """
-        chat_completion = client.chat.completions.create(
-            model=self.model_name,
-            messages=messages,
-            response_format={"type": "json_object"} if json_mode else None,
-            temperature=0,
-        )
-        response = chat_completion.choices[0].message.content
-        return response, chat_completion
+        if "gpt" in self.model_name:
+            chat_completion = self.client.chat.completions.create(
+                model=self.model_name,
+                messages=messages,
+                response_format={"type": "json_object"} if json_mode else None,
+                temperature=0,
+            )
+            response = chat_completion.choices[0].message.content
+            return response, chat_completion
+        else:
+            # Assuming HF Inf models by default
+            chat_completion = self.client.chat_completion(
+                messages=messages,
+                response_format={"type": "json_object"} if json_mode else None,
+                temperature=0,
+            )
+            response = chat_completion.choices[0].message.content
+            return response, chat_completion
 
     def one_step_chat(
         self, text, system_msg: str = None, json_mode=False
@@ -44,6 +59,7 @@ class GPT4V_Client:
     def __init__(self, model_name: str = "gpt-4o", max_tokens: int = 512):
         self.model_name = model_name
         self.max_tokens = max_tokens
+        self.client = OpenAI()
 
     def encode_image(self, path: str):
         with open(path, 'rb') as f:
@@ -65,7 +81,7 @@ class GPT4V_Client:
                     "image_url": {"url": f"data:image/jpeg;base64,{jpg_base64_str}"},},
                 ],
         }]
-        response = client.chat.completions.create(
+        response = self.client.chat.completions.create(
             model=self.model_name,
             messages=messages,
             max_tokens=self.max_tokens,
@@ -77,4 +93,6 @@ CLIENT_DICT = {
     "gpt-3.5-turbo": LM_Client,
     "gpt-4": LM_Client,
     "gpt-4o": GPT4V_Client,
+    "meta-llama/Meta-Llama-3.1-70B-Instruct": LM_Client,
+    "meta-llama/Meta-Llama-3.1-8B-Instruct": LM_Client
 }
